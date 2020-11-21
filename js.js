3b6994d217397e6e78bdf2a1a00ef52e859a6d9f
MoviU <mkachimov@gmail.com>
2020-11-21 22:52:39 +0200

    Add good slider

--- js.js
+++ js.js
@@ -1,20 +1,4 @@
 $(document).ready(function(){
-    $('.carousel').carousel();
-    });
-$('.carousel.carousel-slider').carousel({fullWidth: true});
-
-// Next slide
-$('.carousel').carousel('next');
-$('.carousel').carousel('next', 3); // Move next n times.
-
-// Previous slide
-$('.carousel').carousel('prev');
-$('.carousel').carousel('prev', 4); // Move prev n times.
-
-// Set to nth slide
-$('.carousel').carousel('set', 4);
-
-// Destroy carousel
-$('.carousel').carousel('destroy');
-
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.slider').slider();
+  });
+  $('.slider').slider({height: 1000});
