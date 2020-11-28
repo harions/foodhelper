d322bf024de465764beef2fe335728adee7bd301
mkachimov@gmail.com <2uf-2020@step.lv>
2020-11-28 16:26:56 +0200

    Revert

--- scripts/reservation-scripts.js
+++ scripts/reservation-scripts.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var things = ["Львів", "Київ", "Харків", "Тернопіль"];
+=======
+var city = ["Львів", "Київ", "Харків", "Тернопіль"];
+>>>>>>> parent of 06d42ec... Adnriy changes on main page
 
 
 function autocomplete(inp, arr) {
